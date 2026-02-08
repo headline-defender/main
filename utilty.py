d37a0e46cc090bdb6fac9c1fa8ba2f6ea8695fcd
head <r.sugi6167@gmail.com>
2026-02-08 17:13:43 +0900

    feat: マップをアイコンで選択できるように修正

--- utilty.py
+++ utilty.py
@@ -6,17 +6,25 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # テキスト読み込み関数
-def read_conf(file_path):
+def read_conf(file_path, detail=False):
     f = open(file_path, 'r', encoding='UTF-8')
 
     data = f.read()
     contents_list = data.split("\n")
 
     contents_dict = [
-        {"value": en, "label": jp, "image_path": os.path.join('static','icon',en + '.png')}
+        {
+            "value": en,
+            "label": jp,
+            "image_path": os.path.join("static", "icon", f"{en}.png"),
+            **(
+                {"detail_path": os.path.join("static", "maps", f"{en}.png")}
+                if detail else {}
+            )
+        }
         for item in contents_list
-        for jp,en in [item.split(",", 1)]
-]
+        for jp, en in [item.split(",", 1)]
+    ]
     f.close()
 
     return contents_dict
